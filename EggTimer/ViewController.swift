6fcae49412d4f597d2abfaae5f0ab576761ef790
mubinbin <dinobunmu@gmail.com>
2024-10-05 22:54:32 -0500

    added count down func when press button

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    let eggTimes: [String: Int] = ["Soft": 5, "Medium": 7, "Hard": 12]
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        let hardness: String! = sender.currentTitle
+        
+        var res = eggTimes[hardness]!
+        
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [weak self] (Timer) in
+            if res > 0 {
+                self?.titleLabel.text = "\(res) sec remaining"
+                res -= 1
+            } else {
+                self?.titleLabel.text = "Times up!!"
+                Timer.invalidate()
+            }
+        }
+        
+    }
 }
